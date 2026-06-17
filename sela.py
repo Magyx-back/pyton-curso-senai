ba9c17d4a330973ec63cd83cc578cb68bce650b5
Allan Cazelato <allancazelmelo@gmail.com>
2026-06-17 14:31:46 -0300

    aula 5 pyton

---
+++ sela.py
@@ -0,0 +1,52 @@
+#1
+
+print('sejam bem vindos!')
+
+
+#2
+valor = True
+print(valor)
+
+#3
+
+resultado = 5.0* 5.69
+print(resultado)
+
+#4
+resultado = 5.0/5.69
+print(resultado)
+
+#5
+resultado = 5.0-5.69
+print(resultado)
+
+#6
+resultado= 5.0//5.69
+print(resultado)
+
+#7
+resultados = 5.0*5.69*6.0*7.0
+
+#8
+numero = 10
+print(numero * 2)
+
+#9
+nome = input('qual e seu nome')
+print('seu nome é ', nome)
+
+email = input('qual e seu email')
+print('seu email é ', email)
+
+idade = int(input('qual e sua idade'))
+print('sua idade é ', idade)
+
+cidade = input('qual e sua cidade')
+print('sua cidade é ', cidade)
+
+graduação = input('qual e sua graduação')
+print('sua graduação é ', graduação)
+
+estado_civil = input('qual e sua estado_civil')
+print('seu estado_civil é ', estado_civil)
+
