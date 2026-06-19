456ab1e62355c539c9365845c8bf58edc6756d29
Allan Cazelato <allancazelmelo@gmail.com>
2026-06-19 17:55:33 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,49 @@
+# exercicio 1 
+
+numero = int(input("1) Digite um número: "))
+
+if numero > 10:
+    print("Número positivo")
+elif numero < 0:
+    print("Número negativo")
+else:
+    print("Número é zero")
+
+    #exercicio 2
+idade = int(input("2) Digite sua idade: "))
+
+if idade >= 16:
+    print("Pode votar")
+else:
+    print("Não pode votar")
+
+    #exercicio 3
+    
+numero = int(input("3) Digite um número: "))
+
+if numero % 2 == 0:
+    print("Número par")
+else:
+    print("Número ímpar")
+
+    #exercicio 4
+
+lado1 = float(input("4) Digite o primeiro lado: "))
+lado2 = float(input("Digite o segundo lado: "))
+lado3 = float(input("Digite o terceiro lado: "))
+
+if lado1 == lado2 == lado3:
+    print("Triângulo Equilátero")
+elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
+    print("Triângulo Isósceles")
+else:
+    print("Triângulo Escaleno")
+
+    #exercicio 5
+    
+
+
+
+
+
+                 
