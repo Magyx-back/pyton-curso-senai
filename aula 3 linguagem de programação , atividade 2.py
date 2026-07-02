c1e3ac0560af4c6d0e06bbcb699ddef57c5f1856
Allan Cazelato <allancazelmelo@gmail.com>
2026-07-01 22:00:00 -0300

    Add files via upload

---
+++ aula 3 linguagem de programação , atividade 2.py
@@ -0,0 +1,9 @@
+#atividade 2
+
+numero1 = int(input("Digite um numero: "))
+numero2 = int(input("Digite outro numero: "))
+
+print("Soma =", numero1 + numero2)
+print("Subtracao =", numero1 - numero2)
+print("Multiplicacao =", numero1 * numero2)
+print("Divisao =", numero1 / numero2)
